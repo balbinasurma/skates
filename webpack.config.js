4a408364f7845c5f73a9c16e47c8bb82120cd348
Presh Rychlewicz <prychlewicz@calibrateconsulting.com>
2019-12-31 16:00:41 +0000

    ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: './src/index.tsx',
 	output: {
 		filename: 'bundle.js',
-		path: __dirname
+		path: path.resolve(__dirname, build)
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
