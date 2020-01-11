f5b7f95011398555616e40badffcaf27fe5a4123
Presh Rychlewicz <prychlewicz@calibrateconsulting.com>
2020-01-11 19:51:53 +0000

    Updates

--- 7.bundle.js
+++ 7.bundle.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{52:function(e,t,a){"use strict";a.r(t);var n=a(46),i=a(21),l=a(0),o=a.n(l),r=a(10),c=a(19),b=a(20);const s=r.a.a`
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{52:function(e,t,n){"use strict";n.r(t);var a=n(46),i=n(21),o=n(0),l=n.n(o),r=n(10),c=n(19),b=n(20);const s=r.a.a`
 	width: 80px;
 	height: ${({headerItemHeight:e})=>e};
 	background: none;
@@ -7,7 +7,8 @@
 	display: flex;
 	align-items: center;
 	justify-content: center;
-`;var h=({icon:e,link:t,label:a})=>{const{headerItemHeight:n}=Object(l.useContext)(c.a);return o.a.createElement(s,{headerItemHeight:n,href:t,target:"_blank",rel:"noopener","aria-label":a},o.a.createElement(b.a,{icon:e,color:"white",size:"3x"}))};const m=r.a.section`
+	cursor: pointer;
+`;var h=({icon:e,link:t,label:n})=>{const{headerItemHeight:a}=Object(o.useContext)(c.a);return l.a.createElement(s,{headerItemHeight:a,href:t,target:"_blank",rel:"noopener","aria-label":n},l.a.createElement(b.a,{icon:e,color:"white",size:"3x"}))};const m=r.a.section`
 	display: flex;
 	min-height: ${({headerItemHeight:e})=>e};
-`;t.default=()=>{const{headerItemHeight:e}=Object(l.useContext)(c.a);let t=[{label:"email",icon:i.b,link:"mailto:balbina.surma@gmail.gmail?subject=Kontakt ze strony internetowej"},{label:"LinkedIn",icon:n.b,link:"https://pl.linkedin.com/in/balbina-surma-5a78a8171"},{label:"Facebook",icon:n.a,link:"http://facebook.com/balbina.surma.1"}];return o.a.createElement(m,{headerItemHeight:e},t.map(({icon:e,link:t,label:a},n)=>o.a.createElement(h,{label:a,icon:e,link:t,key:n})))}}}]);
+`;t.default=()=>{const{headerItemHeight:e}=Object(o.useContext)(c.a);let t=[{label:"email",icon:i.b,link:"mailto:balbina.surma@gmail.com?subject=Kontakt ze strony internetowej"},{label:"LinkedIn",icon:a.b,link:"https://pl.linkedin.com/in/balbina-surma-5a78a8171"},{label:"Facebook",icon:a.a,link:"http://facebook.com/balbina.surma.1"}];return l.a.createElement(m,{headerItemHeight:e},t.map(({icon:e,link:t,label:n},a)=>l.a.createElement(h,{label:n,icon:e,link:t,key:a})))}}}]);
