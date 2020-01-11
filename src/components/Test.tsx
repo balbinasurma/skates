5b2f9a7a4308116023e23fe1d98f235d44a09c7b
Presh Rychlewicz <prychlewicz@calibrateconsulting.com>
2020-01-11 17:02:51 +0000

    misc ts

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,30 +1,30 @@
-import React, {useRef} from 'react'
-import styled, {keyframes} from 'styled-components'
+import React from 'react'
+import styled from 'styled-components'
 
-const bounce = keyframes`
-from,
-20%,
-53%,
-80%,
-to {
-  animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-  transform: translate3d(0, 0, 0);
-}
+// const bounce = keyframes`
+// from,
+// 20%,
+// 53%,
+// 80%,
+// to {
+//   animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+//   transform: translate3d(0, 0, 0);
+// }
 
-40%,
-43% {
-  animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
-  transform: translate3d(0, -30px, 0);
-}
+// 40%,
+// 43% {
+//   animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+//   transform: translate3d(0, -30px, 0);
+// }
 
-70% {
-  animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
-  transform: translate3d(0, -15px, 0);
-}
+// 70% {
+//   animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+//   transform: translate3d(0, -15px, 0);
+// }
 
-90% {
-  transform: translate3d(0, -4px, 0);
-}`
+// 90% {
+//   transform: translate3d(0, -4px, 0);
+// }`
 
 const TestSection = styled.section`
 	border: 1px solid red;
@@ -46,7 +46,7 @@ const TestSection = styled.section`
 `
 
 const Test = () => {
-	const testSVGRef = useRef()
+	// const testSVGRef = useRef()
 
 	return (
 		<TestSection>
