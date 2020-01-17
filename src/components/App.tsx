89e54a9b7cf34df5a62ef9886f40cb2f804c1439
Presh Rychlewicz <prychlewicz@calibrateconsulting.com>
2020-01-17 18:03:44 +0000

    misc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,42 +1,39 @@
-import {faAngleDoubleDown} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import React, {lazy, Suspense, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import {Route, Switch} from 'react-router-dom'
-import styled, {keyframes} from 'styled-components'
+import styled from 'styled-components'
 import {routesArrayType} from 'types/allTypes'
 import theme from '../context/theme'
 import themeContext from '../context/themeContext'
 import injectTagInHtml from '../functions/injectTagInHtml'
 import {styledMainType} from '../types/styledTypes'
+import Header from './header/Header'
+import Main from './routes/main/Main'
 
-const Main = lazy(() => import('./routes/main/Main')),
-	Header = lazy(() => import('./header/Header'))
+// const bounce = keyframes`
+// from,
+// 20%,
+// 53%,
+// 80%,
+// to {
+//   animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+//   transform: translate3d(0, 0, 0);
+// }
 
-const bounce = keyframes`
-from,
-20%,
-53%,
-80%,
-to {
-  animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-  transform: translate3d(0, 0, 0);
-}
+// 40%,
+// 43% {
+//   animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+//   transform: translate3d(0, -30px, 0);
+// }
 
-40%,
-43% {
-  animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
-  transform: translate3d(0, -30px, 0);
-}
+// 70% {
+//   animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+//   transform: translate3d(0, -15px, 0);
+// }
 
-70% {
-  animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
-  transform: translate3d(0, -15px, 0);
-}
-
-90% {
-  transform: translate3d(0, -4px, 0);
-}
-`
+// 90% {
+//   transform: translate3d(0, -4px, 0);
+// }
+// `
 
 const AppMain: styledMainType = styled.main`
 	min-height: 100vh;
@@ -46,13 +43,13 @@ const AppMain: styledMainType = styled.main`
 	flex-direction: column;
 	position: relative;
 
-	& > aside {
+	/* & > aside {
 		position: fixed;
 		right: 51px;
 		bottom: 51px;
 		animation: ${bounce} 2s linear infinite;
 		transform-origin: center bottom;
-	}
+	} */
 `
 
 const App = () => {
@@ -70,21 +67,19 @@ const App = () => {
 	return (
 		<AppMain id='app'>
 			<themeContext.Provider value={theme}>
-				<Suspense fallback={<p>...</p>}>
-					<Header />
-				</Suspense>
+				<Header />
 
 				<Switch>
 					{routesArray.map(({path, component}, index) => (
 						<Route exact path={path} key={index}>
-							<Suspense fallback={<p>...</p>}>{component}</Suspense>
+							{component}
 						</Route>
 					))}
 				</Switch>
-
+				{/* 
 				<aside>
 					<FontAwesomeIcon icon={faAngleDoubleDown} color='white' size='4x' />
-				</aside>
+				</aside> */}
 			</themeContext.Provider>
 		</AppMain>
 	)
