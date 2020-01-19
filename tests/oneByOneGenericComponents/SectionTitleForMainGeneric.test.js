c5d251fdc556bd2fef111e99ed7bad9e6f95c530
grandalf6@gmail.com <grandalf6@gmail.com>
2020-01-19 22:41:21 +0000

    misc test

--- tests/oneByOneGenericComponents/SectionTitleForMainGeneric.test.js
+++ tests/oneByOneGenericComponents/SectionTitleForMainGeneric.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {shallow} from 'enzyme'
-import 'jest-styled-components'
 
 import SectionTitleForMainGeneric from '../../src/components/generic/sectionForMain/SectionTitleForMainGeneric'
 
